bea14e15b0e9fcb73b10ad4761d80cb0331f2a39
M6xxy <79139144+M6xxy@users.noreply.github.com>
2021-05-05 15:53:03 +0100

    Added Pause Menu + Changes to Player
    +Pause Menu
    +Changed the Player to a gameobject with a child sprite renderer this will make coding scripts for the player easier in the future as well as it looks better

---
+++ Placeholder Group Project/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool isPaused;
+    public GameObject PauseMenu;
+    AudioSource audiosource;
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+    void Start()
+    {
+        isPaused = false;
+        PauseMenu.SetActive(false);
+        audiosource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                UnPauseGameOnDown();
+                audiosource.volume = 1f;
+            }
+            else
+            {
+                PauseGameOnDown();
+                audiosource.volume = 0f;
+            }
+        }
+    }
+
+    void PauseGameOnDown()
+    {
+        isPaused = true;
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+
+    void UnPauseGameOnDown()
+    {
+        isPaused = false;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        
+    }
+
+}
